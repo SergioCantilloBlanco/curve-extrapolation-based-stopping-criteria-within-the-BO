30ef28e7ada70aa2348e6857a9291cbbcb0555d9
SergioCantilloBlanco <14sergiocb@gmail.com>
2026-04-15 16:30:54 +0200

    feat: Implementation of plot and acquisition functions

--- bayesian_optimizer.py
+++ bayesian_optimizer.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+from acquisition_function import acquisition_ucb
 from surrogate_model import SugorrateModel
 
 
 class BayesianOptimizer:
 
-    def __init__(self,bounds, num_iter, surrogate, acquisition = None, stopping = None ) -> None:
+    def __init__(self,bounds,num_samples,num_iter, surrogate, acquisition, stopping = None ) -> None:
         self.bounds = bounds
         self.num_iter = num_iter
+        self.num_samples = num_samples
         self.surrogate = surrogate
         self.acquisition = acquisition
         self.stopping = stopping
@@ -18,23 +20,42 @@ class BayesianOptimizer:
         self.y = []
 
     def initialize(self, objective):
-        for _ in range(self.num_iter):
+        for _ in range(self.num_samples):
             #x = np.random.uniform([b[0] for b in self.bounds], [b[1] for b in self.bounds])
             x = np.random.uniform(self.bounds[0], self.bounds[-1])
             y = objective(x)
 
-            self.X.append(x)
-            self.y.append(y)
+            self.X = np.append(self.X, x)
+            self.y = np.append(self.y, y)
+    
+    def plot(self, y_pred, y_std,new_x,new_y,i):
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(self.bounds, black_box_function(self.bounds), label='Black Box Function')
+        ax.scatter(self.X, self.y, color='red', label='Samples')
+        ax.scatter(new_x, new_y, color='blue', label='New point')
+        ax.plot(self.bounds, y_pred, color='blue', label='Gaussian Process')
+        ax.fill_between(self.bounds, y_pred - 2*y_std, y_pred + 2*y_std, color='blue', alpha=0.2)
+        ax.set(title=f'Black Box Function with Gaussian Process Surrogate Model Loop Nº{i}', ylabel='Black Box Output', xlabel='x')
+        plt.show()
+
 
     def loop(self):
-        self.surrogate.fit(self.X, self.y)
+        for i in range(self.num_iter):
+            self.surrogate.fit(self.X, self.y)
 
-        best_y = np.max(self.y)
+            #y_pred, y_std = self.surrogate.predict(np.random.uniform([b[0] for b in self.bounds], [b[1] for b in self.bounds]))
+            # sample_x = np.random.choice(x_range, size=self.num_iter).reshape(-1,1)
 
-        #y_pred, y_std = self.surrogate.predict(np.random.uniform([b[0] for b in self.bounds], [b[1] for b in self.bounds]))
-        # sample_x = np.random.choice(x_range, size=self.num_iter).reshape(-1,1)
-        y_pred, y_std = self.surrogate.predict(self.bounds.reshape(-1,1))
-        return y_pred, y_std
+            y_pred, y_std = self.surrogate.predict(self.bounds.reshape(-1,1))
+            ucb = self.acquisition(y_pred, y_std)
+
+            new_x = self.bounds[np.argmax(ucb)]
+            new_y = black_box_function(new_x)
+            self.X = np.append(self.X, new_x)
+            self.y = np.append(self.y, new_y)
+            self.plot(y_pred,y_std,new_x,new_y,i)
+
+
 
 def black_box_function(x):
     y = np.sin(x) + np.cos(2*x)
@@ -43,21 +64,9 @@ def black_box_function(x):
 if __name__ == "__main__":
     x_range = np.linspace(-2*np.pi, 2*np.pi, 200)
     surrogate_model = SugorrateModel()
-    optimizer = BayesianOptimizer(x_range,15,surrogate_model)
+    optimizer = BayesianOptimizer(x_range,15,15,surrogate_model,acquisition_ucb)
     optimizer.initialize(black_box_function)
-
-    y_pred, y_std = optimizer.loop()
-    plt.figure(figsize=(10, 6))
-    plt.plot(x_range, black_box_function(x_range), label='Black Box Function')
-    plt.scatter(optimizer.X, optimizer.y, color='red', label='Samples')
-    plt.plot(x_range, y_pred, color='blue', label='Gaussian Process')
-    plt.fill_between(x_range, y_pred - 2*y_std, y_pred + 2*y_std, color='blue', alpha=0.2)
-
-    plt.xlabel('x')
-    plt.ylabel('Black Box Output')
-    plt.title('Black Box Function with Gaussian Process Surrogate Model')
-    plt.legend()
-    plt.show()
+    optimizer.loop()
 
 
 
--- surrogate_model.py
+++ surrogate_model.py
@@ -1,18 +1,21 @@
 
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel
 
 
 class SugorrateModel:
 
     def __init__(self):
-        kernel = RBF(1.0)
-        self.model = GaussianProcessRegressor(kernel=kernel)
+        #kernel = RBF(1.0)
+        kernel = ConstantKernel(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3))
+        self.model = GaussianProcessRegressor(kernel=kernel,
+            alpha=1e-6,)
 
     def fit(self, X, y):
         X = np.array(X).reshape(-1,1)
         self.model.fit(X, y)
 
     def predict(self, X):
+        X = np.array(X).reshape(-1,1)
         return self.model.predict(X, return_std=True)
