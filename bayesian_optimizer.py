5c8073908fa07c97c8845adfa7dccf761a54e7e2
SergioCantilloBlanco <14sergiocb@gmail.com>
2026-06-05 17:53:36 +0200

    refactor: :recycle: added recorded points dataclass as a storage dataclass for the points

--- bayesian_optimizer.py
+++ bayesian_optimizer.py
@@ -4,6 +4,7 @@ from scipy.optimize import curve_fit
 
 from acquisition_function import acquisition_lcb
 from functions import *
+from recorded_points import RecordedPoints
 from surrogate_model import SurrogateModel
 
 
@@ -18,9 +19,7 @@ class BayesianOptimizer:
         self.acquisition = acquisition
         self.stopping = stopping
         self.epsilon = epsilon
-        self.best_value_y = []
-        self.X = []
-        self.y = []
+        self.recorded_points = RecordedPoints()
 
     def initialize(self):
         for _ in range(self.num_samples):
@@ -28,13 +27,11 @@ class BayesianOptimizer:
             #x = np.random.uniform(self.bounds[0], self.bounds[-1])
             y = self.objective(x)
 
-            self.X.append(x)
-            self.y.append(y)
-            self.best_value_y.append(np.min(self.y))
+            self.recorded_points.add(x,y)
 
     def step(self):
-        X = np.array(self.X)
-        y = np.array(self.y)
+        X = np.array(self.recorded_points.X)
+        y = np.array(self.recorded_points.y)
 
         self.surrogate.fit(X, y)
 
@@ -45,9 +42,8 @@ class BayesianOptimizer:
         new_x = candidates[np.argmin(ucb)]
         new_y = self.objective(new_x)
 
-        self.X.append(new_x)
-        self.y.append(new_y)
-        self.best_value_y.append(np.min(self.y))
+
+        self.recorded_points.add(new_x,new_y)
 
         return {"new_x": new_x, "new_y": new_y, "y_pred": y_pred, "y_std": y_std}
 
@@ -56,13 +52,13 @@ class BayesianOptimizer:
     def oned_plot(self, y_pred, y_std,new_x,new_y,i):
         fig, (ax1, ax2) = plt.subplots(nrows=1,ncols=2,figsize=(10, 5))
         ax1.plot(self.bounds, self.objective(self.bounds), label='Black Box Function')
-        ax1.scatter(self.X, self.y, color='red', label='Samples')
+        ax1.scatter(self.recorded_points.X, self.recorded_points.y, color='red', label='Samples')
         ax1.scatter(new_x, new_y, color='blue', label='New point')
         ax1.plot(self.bounds, y_pred, color='blue', label='Gaussian Process')
         ax1.fill_between(self.bounds, y_pred - 2*y_std, y_pred + 2*y_std, color='blue', alpha=0.2)
         ax1.set(title=f'Black Box Function with Gaussian Process Surrogate Model Loop Nº{i}', ylabel='Black Box Output', xlabel='x')
-        ax2.plot(np.arange(len(self.best_value_y)), self.best_value_y,color='orange', label='convergence curve')
-        ax2.scatter(np.arange(len(self.best_value_y)), self.best_value_y, color='blue', label='samples')
+        ax2.plot(np.arange(len(self.recorded_points.best_y)), self.recorded_points.best_y,color='orange', label='convergence curve')
+        ax2.scatter(np.arange(len(self.recorded_points.best_y)), self.recorded_points.best_y, color='blue', label='samples')
 
     def multid_plot(self, y_pred, y_std,new_x,new_y,optimal_values,i):
         fig, (ax1, ax2) = plt.subplots(nrows=1,ncols=2,figsize=(10, 5))
@@ -85,15 +81,15 @@ class BayesianOptimizer:
 
         ax1.fill_between(x_plot,y_pred - 2*y_std,y_pred + 2*y_std,alpha=0.2)
 
-        X = np.array(self.X)
-        ax1.scatter(X[:, 0], self.y, color='red', label='Samples')
+        X = np.array(self.recorded_points.X)
+        ax1.scatter(X[:, 0], self.recorded_points.y, color='red', label='Samples')
         ax1.scatter([new_x[0]], [new_y], color='blue', label='New point')
         ax1.set_title(f'Iteration {i}')
         ax1.legend()
 
-        extrapolated_curve_x = np.linspace(0,len(self.best_value_y)+5,200)
+        extrapolated_curve_x = np.linspace(0,len(self.recorded_points.best_y)+5,200)
         extrapolated_curve_y = exp_decay(extrapolated_curve_x, *optimal_values)
-        ax2.plot(self.best_value_y, color='orange', label="convergence curve")
+        ax2.plot(self.recorded_points.best_y, color='orange', label="convergence curve")
         ax2.plot(extrapolated_curve_x, extrapolated_curve_y, color='blue', linestyle = 'dashed', label="extrapolated curve")
         ax2.set_title("Convergence")
         ax2.legend()
@@ -105,13 +101,13 @@ class BayesianOptimizer:
 
             results = self.step()
 
-            x_data_convergence_curve = np.arange(len(self.best_value_y))
-            a = self.best_value_y[0] - self.best_value_y[-1]
+            x_data_convergence_curve = np.arange(len(self.recorded_points.best_y))
+            a = self.recorded_points.best_y[0] - self.recorded_points.best_y[-1]
             b = 0.1
-            c = self.best_value_y[-1]
+            c = self.recorded_points.best_y[-1]
             try:
-                if len(self.best_value_y) > 10:
-                    optimal_values, covariance = curve_fit(exp_decay, x_data_convergence_curve, self.best_value_y,p0=[a,b,c], bounds = ([0, 0, self.best_value_y[-1] - self.best_value_y[0] + self.best_value_y[-1]], [self.best_value_y[0], np.inf, self.best_value_y[-1]]), maxfev=10000)
+                if len(self.recorded_points.best_y) > 10:
+                    optimal_values, covariance = curve_fit(exp_decay, x_data_convergence_curve, self.recorded_points.best_y,p0=[a,b,c], bounds = ([0, 0, self.recorded_points.best_y[-1] - self.recorded_points.best_y[0] + self.recorded_points.best_y[-1]], [self.recorded_points.best_y[0], np.inf, self.recorded_points.best_y[-1]]), maxfev=10000)
                     percentage_gain = ((c - optimal_values[-1]) / c)*100
                     print(f"Percentage gain:{percentage_gain}")
                     if self.stopping:
